e7f4bdbcddfd7a89bff0058171448b0c583f6eb4
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-23 13:36:10 +0200

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,13 +1,7 @@
-// module.exports.signup_get = (req, res) => {
-//     res.render('signup');
-// }
+
 module.exports.signup_post = (req, res) => {
 
 }
-
-// module.exports.login_get = (req, res) => {
-//     res.render('signup');
-// }
 
 module.exports.login_post = (req, res) => {
 
