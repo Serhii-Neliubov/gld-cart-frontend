47e9921e2a20ee14277b05bd6dbb0fa9a2735476
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-23 13:31:24 +0200

    Implement basic MVC

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
+const authRoutes = require('./routes/authRoutes')
 
 const server = express();
 
-const dbURI = 'mongodb+srv://kolya_228:oD5TAoabAZyJDOhr@cluster0.iaw7xzn.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(dbURI)
-  .then((result) => server.listen(3000))
-  .catch((err) => console.log(err));
 
+const dbURI = 'mongodb+srv://kolya_228:oD5TAoabAZyJDOhr@cluster0.iaw7xzn.mongodb.net/node-auth';
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then((result) => server.listen(3000))
+    .catch((err) => console.log(err));
+
+server.use(cors());
+server.use(authRoutes);
