724f9201f4be9c258ca155891ed014fadfcc2e5b
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-09-23 14:30:38 +0200

    add auth check

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,12 +1,63 @@
+import { useState } from "react";
 import Login from "../../components/UI/Login";
 import styles from "./RegisterPage.module.scss";
 import { Link } from "react-router-dom";
+
+interface IUser {
+  type: string;
+  name: string;
+  surname: string;
+  password: string;
+  rePassword: string;
+}
 
 interface RegisterPageProps {
   isVendorType: boolean;
 }
 
 const RegisterPage = ({ isVendorType }: RegisterPageProps) => {
+  const [userData, setUserData] = useState({
+    type: "",
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    rePassword: "",
+  });
+  function sendFormData(formData: IUser) {
+    fetch("/your-api-endpoint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Ответ сервера:", data);
+      })
+      .catch((error) => {
+        console.error("Ошибка при отправке данных:", error);
+      });
+  }
+  function sendFormHandler(): void {
+    if (isVendorType) {
+      setUserData({ ...userData, type: "Vendor" });
+    } else {
+      setUserData({ ...userData, type: "Buyer" });
+    }
+
+    if (
+      userData.password === userData.rePassword &&
+      userData.name.length &&
+      userData.password.length &&
+      userData.surname.length &&
+      userData.rePassword.length
+    ) {
+      sendFormData(userData);
+    }
+  }
+
   return (
     <div className={styles.body}>
       <Login>
@@ -28,24 +79,59 @@ const RegisterPage = ({ isVendorType }: RegisterPageProps) => {
                 <div className={styles.form_column_wrap}>
                   <div className={styles.input}>
                     <span>First Name</span>
-                    <input type="text" placeholder="ex:John" />
+                    <input
+                      onChange={(e) =>
+                        setUserData({ ...userData, name: e.target.value })
+                      }
+                      value={userData.name}
+                      type="text"
+                      placeholder="ex:John"
+                    />
                   </div>
                   <div className={styles.input}>
                     <span>Last Name</span>
-                    <input type="text" placeholder="ex:Miller" />
+                    <input
+                      onChange={(e) =>
+                        setUserData({ ...userData, surname: e.target.value })
+                      }
+                      value={userData.surname}
+                      type="text"
+                      placeholder="ex:Miller"
+                    />
                   </div>
                 </div>
                 <div className={styles.input}>
                   <span>Your Email</span>
-                  <input type="email" placeholder="Gldcart@mail.com" />
+                  <input
+                    onChange={(e) =>
+                      setUserData({ ...userData, email: e.target.value })
+                    }
+                    value={userData.email}
+                    type="email"
+                    placeholder="Gldcart@mail.com"
+                  />
                 </div>
                 <div className={styles.input}>
                   <span>Password</span>
-                  <input type="password" placeholder="Min. 6 character" />
+                  <input
+                    onChange={(e) =>
+                      setUserData({ ...userData, password: e.target.value })
+                    }
+                    value={userData.password}
+                    type="password"
+                    placeholder="Min. 6 character"
+                  />
                 </div>
                 <div className={styles.input}>
                   <span>Re-Password</span>
-                  <input type="password" placeholder="Min. 6 character" />
+                  <input
+                    onChange={(e) =>
+                      setUserData({ ...userData, rePassword: e.target.value })
+                    }
+                    value={userData.rePassword}
+                    type="password"
+                    placeholder="Min. 6 character"
+                  />
                 </div>
               </div>
             </form>
@@ -54,12 +140,9 @@ const RegisterPage = ({ isVendorType }: RegisterPageProps) => {
             <input type="checkbox" />
             <span>Remember me</span>
           </div>
-          <Link
-            className={styles.button}
-            to={isVendorType ? "/sub-plans" : "/"}
-          >
+          <button onClick={sendFormHandler} className={styles.button}>
             Create my account
-          </Link>
+          </button>
         </div>
       </Login>
     </div>
