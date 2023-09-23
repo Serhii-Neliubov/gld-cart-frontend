0f688ec5467c3891d25d5c3a642270348b93b5bb
sappChak <konotop401@gmail.com>
2023-09-23 12:30:30 +0200

    Add controllers folder

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,3 +2,10 @@ const {Router} = require('express');
 
 const router = Router();
 
+router.get('/signup', () => {});
+router.post('/signup', () => {});
+
+router.get('/login', () => {});
+router.post('/login', () => {});
+
+module.exports = router;
