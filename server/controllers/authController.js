2689b17a47cbd5ece766e40c0e04458cc600a00a
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-24 09:29:46 +0200

    Add client's type & client's name and surname

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,15 +2,15 @@ const User = require('../models/User')
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
-    let errors = { email: '', password: ''};
+    let errors = { email: '', password: '' };
 
-    if(err.code == 11000){
+    if (err.code == 11000) {
         errors.email = "This email is already taken"
     }
 
-    if(err.message.includes('user validation failed')) {
-       Object.values(err.errors).forEach(({properties}) => {
-        errors[properties.path] = properties.message;
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
         });
     }
     return errors;
@@ -18,18 +18,18 @@ const handleErrors = (err) => {
 
 module.exports.signup_post = async (req, res) => {
 
-    const { email, password } = req.body;
+    const { type, name, surname, email, password } = req.body;
 
     try {
 
-        const user = await User.create({ email, password });
+        const user = await User.create({ type, name, surname, email, password });
         res.status(201).json(user);
 
     } catch (error) {
         const errors = handleErrors(error);
-        res.status(400).json( {errors} )
-
+        res.status(400).json({ errors })
     }
+
 }
 
 module.exports.login_post = async (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -2,19 +2,33 @@ const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: [true, "Client' type is undefined" ],
+    },
+    name: {
+        type: String,
+        required: [true, 'Please, enter your first name'],
+    },
+    surname: {
+        type: String,
+        required: [true, 'Please, enter your last name'],
+
+    },
+
     email: {
         type: String,
         required: [true, 'Please, enter an email'],
         unique: true,
         lowercase: true,
-        validate: [ isEmail, 'Please, enter a valid email'] 
+        validate: [isEmail, 'Please, enter a valid email']
     },
     password: {
         type: String,
         required: [true, 'Please, enter a password'],
         minlength: [6, 'Minimum password length is 6 charachters']
     }
- })
+})
 
- const User = mongoose.model('user', userSchema);
- module.exports = User;
+const User = mongoose.model('user', userSchema);
+module.exports = User;
