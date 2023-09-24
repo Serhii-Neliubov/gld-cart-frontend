5c27045b9623c358d03a530f8c13aa83f14e7bd6
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-24 16:28:06 +0200

    Add jwt token to response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
@@ -16,6 +17,11 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 *60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'gld cart secret', { expiresIn: maxAge });
+}
+
 module.exports.signup_post = async (req, res) => {
 
     const { type, name, surname, email, password } = req.body;
@@ -23,7 +29,12 @@ module.exports.signup_post = async (req, res) => {
     try {
 
         const user = await User.create({ type, name, surname, email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000} );
+        
+
+        res.status(201).json({ user: user._id });
 
     } catch (error) {
         const errors = handleErrors(error);
