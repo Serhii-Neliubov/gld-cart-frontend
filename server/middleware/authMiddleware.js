f98d5ac08ef2307be438dcc24af6f12872ec4de4
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-25 17:45:57 +0200

    Add jwt check

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, 'gld cart secret', (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+            }
+            else {
+                console.log(decodedToken);
+                next();
+            }
+        })
+
+    }
+    else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = { requireAuth };
