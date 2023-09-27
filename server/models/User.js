098fa769790c208d267f53d95d0bf79ad0d3f333
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-27 13:13:12 +0200

    Refactor

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,6 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please, enter your last name'],
 
     },
-
     email: {
         type: String,
         required: [true, 'Please, enter an email'],
@@ -36,21 +35,6 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
 } )
-
-userSchema.statics.login = async function(email, password) {
-
-    const user = await this.findOne({ email });
-
-    if(user){
-        const auth = await bcrypt.compare(password, user.password);
-        if(auth) {
-            return user;
-        }
-        throw Error('incorrect password');
-
-    }
-    throw Error('incorrect email');
-}
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
