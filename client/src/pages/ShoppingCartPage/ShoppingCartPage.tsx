512879c5aae0fd2170e8a053acf28c6928639138
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-09-28 14:23:20 +0200

    some changing

--- client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
+++ client/src/pages/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
 import styles from "./ShoppingCartPage.module.scss";
+import SavedItems from "../../utils/SavedItems";
+import { useState } from "react";
 
 const ShoppingCartPage = () => {
+  const [result, setResult] = useState(0);
+
   return (
     <>
       <div className={styles.body}>
@@ -11,7 +14,19 @@ const ShoppingCartPage = () => {
             <span>Home</span>
             <span>Shopping Cart</span>
           </div>
-          <div className={styles.content}></div>
+          <div className={styles.content}>
+            <div className={styles.items}>
+              {SavedItems.map((item) => {
+                return <div>123</div>;
+              })}
+            </div>
+            <div className={styles.sidebar}>
+              <div className={styles.total}>
+                <h2>Subtotal</h2>
+                <span>{result}</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
