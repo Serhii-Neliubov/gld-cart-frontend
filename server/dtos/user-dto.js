5adb5343619cd7ca4e85cce9a9bdab67e267ace1
Andrew <38425829+sappChak@users.noreply.github.com>
2023-09-28 18:04:16 +0200

    Small changes

--- server/dtos/user-dto.js
+++ server/dtos/user-dto.js
@@ -1,10 +1,14 @@
 module.exports = class UserDto {
     id;
+    type;
     name;
+    surname;
     email;
     constructor(model) {
         this.id = model._id;
+        this.type = model.type;
         this.name = model.name;
+        this.surname = model.surname;
         this.email = model.email;
     }
 }
