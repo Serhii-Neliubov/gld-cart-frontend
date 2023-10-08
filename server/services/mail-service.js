9fe55b2903f66b294af91aba077aa62ea2df01b9
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-08 10:12:19 +0200

    Small changes

---
+++ server/services/mail-service.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_USER_PASSWORD,
+      },
+    });
+  }
+  async sendContactMail(name, email, subject, message, to) {
+    await this.transporter.sendMail({
+      from: "GLDCart Feedback",
+      to: to,
+      subject: subject,
+      text:
+        "My name is: " + name + ". " + "My email is: " + email + ". " + message,
+    });
+  }
+}
+module.exports = new MailService();
