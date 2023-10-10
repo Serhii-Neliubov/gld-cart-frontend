877231395d965ec8fb683977852b757cab38b073
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-10 18:58:01 +0200

    Remove "userSchema.pre" method

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,33 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: [true, "Client's type is undefined" ],
-    },
-    name: {
-        type: String,
-        required: [true, 'Please, enter your first name'],
-    },
-    surname: {
-        type: String,
-        required: [true, 'Please, enter your last name'],
+  type: {
+    type: String,
+    required: [true, "Client's type is undefined"],
+  },
+  name: {
+    type: String,
+    required: [true, "Please, enter your first name"],
+  },
+  surname: {
+    type: String,
+    required: [true, "Please, enter your last name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please, enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please, enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please, enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
+});
 
-    },
-    email: {
-        type: String,
-        required: [true, 'Please, enter an email'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'Please, enter a valid email']
-    },
-    password: {
-        type: String,
-        required: [true, 'Please, enter a password'],
-        minlength: [6, 'Minimum password length is 6 charachters']
-    }
-})
-
-userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-} )
-
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 module.exports = User;
