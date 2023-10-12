90e238781b320cbc0d271bf80b00a0a2f7ddf899
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-12 15:00:01 +0200

    Implement sending reset password email

--- server/services/mail-service.js
+++ server/services/mail-service.js
@@ -18,5 +18,28 @@ class MailService {
       text: `My name is: ${name}. My email is: ${email}. ${message}`,
     });
   }
+  async sendResetPasswordMail(email, link) {
+    await this.transporter.sendMail({
+      from: "GLDCart",
+      to: email,
+      subject: "Password reset on GLDCart.com",
+      html: `<!DOCTYPE html>
+           <html>
+           <head>
+           <meta charset="UTF-8">
+           <title>Password Reset</title>
+           </head>
+           <body>
+           <p>Hello,</p>
+           <p>You have requested to reset your password. To reset your password, please click on the following link:</p>
+           <p><a href="${link}">Reset Password</a></p>
+           <p>If you did not request a password reset, please ignore this email.</p>
+           <p>Thank you!</p>
+           </body>
+           </html>
+`,
+    });
+  }
 }
+
 module.exports = new MailService();
