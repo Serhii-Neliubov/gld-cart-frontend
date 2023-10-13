f9bb45875acbd57c4fcbf5e5c46569ed0b9ddf56
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-13 16:48:55 +0200

    Add /api/ to the routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,14 +5,14 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const paymentController = require("../controllers/paymentController");
 
 //Auth routes
-router.post("/signup", userController.signup_post);
-router.post("/login", userController.login_post);
-router.post("/logout", userController.logout_post);
-router.get("/refresh", userController.refresh_get);
+router.post("/api/signup", userController.signup_post);
+router.post("/api/login", userController.login_post);
+router.post("/api/logout", userController.logout_post);
+router.get("/api/refresh", userController.refresh_get);
 
 //Reset password routes
-router.post('/forgot-password', userController.initiate_password_reset);
-router.post('/reset-password/:token', userController.reset_password);
+router.post('/api/forgot-password', userController.initiate_password_reset);
+router.post('/api/reset-password/:token', userController.reset_password);
 
 //Admin routes
 router.get(
