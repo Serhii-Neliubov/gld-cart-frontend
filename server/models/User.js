b8a82783452ffd1582ac1372d1ee557faf812583
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-13 10:46:50 +0200

    Add passwordResetToken field

--- server/models/User.js
+++ server/models/User.js
@@ -27,6 +27,9 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please, enter a password"],
     minlength: [6, "Minimum password length is 6 characters"],
   },
+  passwordResetToken: {
+    type: String
+  }
 });
 
 const User = mongoose.model("user", userSchema);
