939c9e58290f054ad63a7dcff00aadca89dfac52
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-26 10:52:37 +0200

    Integrate socket.io

--- server/server.js
+++ server/server.js
@@ -5,28 +5,40 @@ const mongoose = require("mongoose");
 const router = require("./routes/router");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middlewares/errorMiddleware");
+const { Server } = require("socket.io");
+const { createServer } = require("node:http");
 const port = process.env.PORT || 5000;
-const server = express();
 
-server.use(express.json());
-server.use(cookieParser());
-server.use(
-    cors({
-        credentials: true,
-        origin: process.env.CLIENT_URL,
-    })
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
 );
-server.use(router);
-server.use(errorMiddleware);
+app.use(router);
+app.use(errorMiddleware);
 
 mongoose
-    .connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then((result) => server.listen(process.env.DB_PORT))
-    .catch((err) => console.log(err));
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => app.listen(process.env.DB_PORT))
+  .catch((err) => console.log(err));
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
