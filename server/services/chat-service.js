6b5db7471f6a07133e1e5093b787c28a77f42cb6
Andrew <38425829+sappChak@users.noreply.github.com>
2023-10-29 17:39:35 +0100

    Get all messages between two users

--- server/services/chat-service.js
+++ server/services/chat-service.js
@@ -6,6 +6,26 @@ class ChatService {
       sender: sender,
       receiver: receiver,
     });
+    await newMessage.save();
+    return newMessage;
+  }
+  async getAllMessages(sender, receiver){
+
+    const messages = await MessageModel.find({
+      $or: [
+        { sender: sender, receiver: receiver },
+        { sender: receiver, receiver: sender }
+      ]
+    }).sort({ updatedAt: 1 });
+
+
+    return messages.map((msg)=> {
+      return{
+        myself: msg.sender.toString() === sender,
+        message: msg.text,
+      }
+    });
+
   }
 }
 module.exports = new ChatService();
