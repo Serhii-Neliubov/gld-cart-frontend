23086682386c50c69caca9d8790a5cae0bdd555b
Andrew <38425829+sappChak@users.noreply.github.com>
2023-11-03 15:26:20 +0100

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,6 +10,7 @@ router.post("/signup", userController.signup_post);
 router.post("/login", userController.login_post);
 router.post("/logout", userController.logout_post);
 router.get("/refresh", userController.refresh_get);
+router.post("/send-contact-email", userController.send_contact_email);
 
 //Reset password routes
 router.post("/forgot-password", userController.initiate_password_reset);
