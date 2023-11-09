aef7b99709d61bcc7e3faea8384c630c737babd7
Andrew <38425829+sappChak@users.noreply.github.com>
2023-11-09 20:48:00 +0100

    Migrate googleController to userController

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -4,12 +4,16 @@ import UserService from "../services/user-service";
 import TokenService from "../services/token-service";
 import { v4 as uuidv4 } from 'uuid';
 const maxAge: number = 30 * 24 * 60 * 60 * 1000;
+import {
+  getGoogleOAuthTokens,
+  getGoogleUser,
+} from "../services/google-service";
 
 export const signup_post = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const { type, name, surname, email, password } = req.body;
 
   try {
@@ -35,7 +39,7 @@ export const login_post = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const { email, password } = req.body;
   try {
     const userData = await UserService.login(email, password);
@@ -87,7 +91,7 @@ export const reset_password = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+) : Promise<void> => {
   const { token: token } = req.params;
   const { newPassword } = req.body;
   try {
@@ -120,20 +124,63 @@ export const send_contact_email = async (
   res: Response,
   next: NextFunction
 ) => {
-    const { name, email, subject, message, token } = req.body;
+  const { name, email, subject, message, token } = req.body;
 
   try {
     const userData = token ? TokenService.validateAccessToken(token) : null;
-    const recipientEmail = email || (userData?.email);
+    const recipientEmail = email || userData?.email;
 
     if (recipientEmail) {
       await MailService.sendContactMail(name, recipientEmail, subject, message);
       return res.json({ success: true, message: "Email sent successfully" });
     }
 
-    return res.status(400).json({ success: false, message: "Error. Email was not sent" });
+    return res
+      .status(400)
+      .json({ success: false, message: "Error. Email was not sent" });
   } catch (e) {
     next(e);
+  }
+};
+export const googleOauthHandler = async (req: Request, res: Response) => {
+  const code: string = req.query.code as string;
+  const customParameter = req.query.state;
+  try {
+    const { id_token, access_token } = await getGoogleOAuthTokens({ code });
+    const googleUser = await getGoogleUser({ id_token, access_token });
+
+    if (!googleUser.verified_email) {
+      return res.status(403).send("Google account is not verified");
+    }
+    const userData = await UserService.loginGoogleUser(
+      code,
+      customParameter,
+      googleUser.name,
+      googleUser.family_name,
+      googleUser.email,
+      googleUser.picture,
+      process.env.USERS_AFTER_GOOGLE_PASSWORD
+    );
+
+    res.cookie("refreshToken", userData.refreshToken, {
+      httpOnly: true,
+      maxAge: maxAge,
+    });
+    res.cookie("accessToken", userData.accessToken, {
+      httpOnly: true,
+      maxAge: maxAge,
+    });
+
+    const redirectURL: string = `${process.env.CLIENT_URL}/
+    ?id=${userData.user.id}
+    &type=${userData.user.type}
+    &name=${userData.user.name}
+    &surname=${userData.user.surname}
+    &email=${userData.user.email}
+    &picture=${userData.picture}`;
+    res.redirect(redirectURL);
+  } catch (error) {
+    return res.redirect(`${process.env.CLIENT_URL}/oauth/error`);
   }
 };
 
