0302331848c94dbb01026726ff887e4fc4491bb5
sappChak <konotop401@gmail.com>
2023-11-30 10:53:05 +0100

    Adjust properties

--- server/models/Product.ts
+++ server/models/Product.ts
@@ -2,22 +2,23 @@
 import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export interface IProduct extends Document {
-  id: string;
+  _id: string;
   name: string;
-  description: string;
+  brand: string;
+  desc: string;
   price: number;
   image: string;
   cartQuantity: number;
 }
 
-const productSchema = new Schema<IProduct>({
+export const productSchema = new Schema<IProduct>({
     name: { type: String, required: true },
-    brand: { type: String, required: true },
-    description: { type: String, required: true },
+    brand: { type: String, required: false },
+    desc: { type: String, required: false },
     price: { type: Number, required: true },
     image: { type: String, required: true },
     cartQuantity: { type: Number},
 });
 
-const Product: Model<IProduct> = mongoose.model('User', productSchema) as Model<IProduct>;
+const Product = mongoose.model('Product', productSchema) as Model<IProduct>;
 export default Product;
