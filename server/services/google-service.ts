5f1447d121940030bdd951080b802e254c4072cd
sappChak <konotop401@gmail.com>
2023-11-30 10:53:46 +0100

    Fix unresolved types

--- server/services/google-service.ts
+++ server/services/google-service.ts
@@ -52,10 +52,10 @@ interface GoogleUserResult {
   locale: string;
 }
 
-export async function getGoogleUser({
-  id_token,
-  access_token,
-}): Promise<GoogleUserResult> {
+export async function getGoogleUser(
+  id_token : string,
+  access_token : string,
+): Promise<GoogleUserResult> {
   try {
     const res = await axios.get<GoogleUserResult>(
       `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
