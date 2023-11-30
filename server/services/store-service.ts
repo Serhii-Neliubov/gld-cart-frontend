b4d1b3b3d9c1094f80a212ecc3cb92c93292259b
sappChak <konotop401@gmail.com>
2023-11-30 10:46:23 +0100

    Resolve problems with types via null checking

--- server/services/store-service.ts
+++ server/services/store-service.ts
@@ -1,6 +1,14 @@
 import Order from "../models/Order";
 
 class StoreService {
+    async createOrder(customer: Stripe.Customer | Stripe.DeletedCustomer, data: Stripe.Event.Data.Object) {
+        if ("metadata" in customer &&
+            "customer" in data &&
+            "payment_intent" in data &&
+            "amount_subtotal" in data &&
+            "amount_total" in data &&
+            "customer_details" in data &&
+            "payment_status" in data) {
 
     async createOrder(customer, data) {
         const Items = JSON.parse(customer.metadata.cart);
@@ -22,6 +30,26 @@ class StoreService {
         }
     };
 
+            const Items = JSON.parse(customer.metadata.cart) as IProduct[];
+            console.log(Items);
+            try {
+                const newOrder = new Order({
+                    userId: customer.metadata.userId,
+                    customerId: data.customer,
+                    paymentIntentId: data.payment_intent,
+                    products: Items,
+                    subtotal: data.amount_subtotal,
+                    total: data.amount_total,
+                    shipping: data.customer_details,
+                    payment_status: data.payment_status,
+                });
+                const savedOrder = await newOrder.save();
+                console.log("Processed Order:", savedOrder);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
 }
 
 export default new StoreService();
