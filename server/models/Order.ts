c3552357c61b8c0ddffca785760c4fc18216007a
sappChak <konotop401@gmail.com>
2023-11-30 10:57:14 +0100

    Use Product's model productSchema

--- server/models/Order.ts
+++ server/models/Order.ts
@@ -1,36 +1,28 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
-import {IProduct}                   from "./Product";
+import mongoose, {Document, Model, Schema} from "mongoose";
+import {IProduct, productSchema} from "./Product";
 
 export interface IOrder extends Document {
-  user: mongoose.Types.ObjectId;
-  products: IProduct[];
-  subtotal: number;
-  total: number;
-  shipping?: object;
-  delivery_status: string;
-  payment_status: string;
+    user: mongoose.Types.ObjectId;
+    products: IProduct[];
+    subtotal: number;
+    total: number;
+    shipping?: object;
+    delivery_status: string;
+    payment_status: string;
 }
-const productSchema = new Schema<IProduct>({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  id: { type: String, required: true },
-  price: { type: Number, required: true },
-  cartQuantity: { type: Number, required: true },
-});
 
 const orderSchema: Schema<IOrder> = new Schema(
-  {
-    user: { type: Schema.Types.ObjectId, ref: 'User', },
-    products: [productSchema],
-    subtotal: { type: Number, required: true },
-    total: { type: Number, required: true },
-    shipping: { type: Object},
-    delivery_status: { type: String, default: 'pending' },
-    payment_status: { type: String, required: true },
-  },
+    {
+        user: {type: Schema.Types.ObjectId, ref: 'User'},
+        products: [productSchema],
+        subtotal: {type: Number, required: true},
+        total: {type: Number, required: true},
+        shipping: {type: Object},
+        delivery_status: {type: String, default: 'pending'},
+        payment_status: {type: String, required: true},
+    },
 );
 
-const Order: Model<IOrder> = mongoose.model('Order', orderSchema) as Model<IOrder>;
+const Order  = mongoose.model('Order', orderSchema) as Model<IOrder>;
 
 export default Order;
