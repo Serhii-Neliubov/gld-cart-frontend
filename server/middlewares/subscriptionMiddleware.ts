79454ad9a9ac2946f95147b8a9992443197a19cb
sappChak <konotop401@gmail.com>
2023-12-17 15:07:03 +0100

    Add subscription model

---
+++ server/middlewares/subscriptionMiddleware.ts
@@ -0,0 +1,31 @@
+import {NextFunction, Request, Response} from "express";
+import ApiError from "../exceptions/api-error";
+import UserModel from "../models/UserModel";
+
+export const requireSubscription = (requiredSubscriptionType: string) => {
+    return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const userId = res.locals.user.userId;
+
+            if (!userId) {
+                return next(ApiError.UnauthorizedError());
+            }
+
+            const user = await UserModel.findById(userId).populate('activeSubscription');
+
+            if (!user || !user.activeSubscription) {
+                res.status(403).json({ error: 'No active subscription found' });
+            }
+
+            const userSubscriptionType = user?.activeSubscription;
+
+            if (userSubscriptionType !== requiredSubscriptionType) {
+                res.status(403).json({ error: 'Access denied for this subscription type' });
+            }
+            return next();
+        } catch (error) {
+            console.error('Error checking subscription:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    };
+};
