06f767bf5d461e125914ab4c7a25aa73802af4d1
sappChak <konotop401@gmail.com>
2023-12-23 15:32:00 +0100

    Create logger class

---
+++ server/src/util/logger.ts
@@ -0,0 +1,93 @@
+import winston, {format} from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+export type LogMessage = string;
+
+export type LogContext = object;
+
+export enum LogLevel {
+    DEBUG = 'debug',
+    INFO = 'info',
+    WARN = 'warn',
+    ERROR = 'error',
+}
+export class Logging {
+    private _logger: winston.Logger;
+    private static _appName = 'GLD Cart';
+
+    constructor() {
+        this._logger = this._initializeWinston();
+    }
+
+    public logInfo(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevel.INFO, context);
+    }
+    public logWarn(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevel.WARN, context);
+    }
+    public logError(msg: LogMessage, context?: LogContext) {
+        this._log(msg, LogLevel.ERROR, context);
+    }
+    public logDebug(msg: LogMessage, context?: LogContext) {
+        if (process.env.NODE_ENV !== 'production') {
+            this._log(msg, LogLevel.DEBUG, context);
+        }
+    }
+
+    private _log(msg: LogMessage, level: LogLevel, context?: LogContext) {
+        this._logger.log(level, msg, {context: context});
+    }
+
+    private _initializeWinston() {
+        return winston.createLogger({
+            transports: Logging._getTransports(),
+        });
+    }
+
+    private static _getTransports() {
+        const transports: Array<any> = [
+            new winston.transports.Console({
+                format: this._getFormatForConsole(),
+            }),
+        ];
+
+        if (process.env.NODE_ENV === 'production') {
+            transports.push(this._getFileTransport());
+        }
+
+        return transports;
+    }
+
+    private static _getFormatForConsole() {
+        return format.combine(
+            format.timestamp(),
+            format.printf(
+                info =>
+                    `[${info.timestamp}] [${info.level.toUpperCase()}]: ${
+                        info.message
+                    } [CONTEXT] -> ${
+                        info.context ? '\n' + JSON.stringify(info.context, null, 2) : '{}' 
+                    }`
+            ),
+            format.colorize({all: true})
+        );
+    }
+
+    private static _getFileTransport() {
+        return new DailyRotateFile({
+            filename: `${Logging._appName}-%DATE%.log`,
+            zippedArchive: true,
+            maxSize: '10m', // Rotate after 10MB
+            maxFiles: '14d', // Only keep last 14 days
+            format: format.combine(
+                format.timestamp(),
+                format(info => {
+                    console.log(info);
+                    info.app = this._appName;
+                    return info;
+                })(),
+                format.json()
+            ),
+        });
+    }
+}
