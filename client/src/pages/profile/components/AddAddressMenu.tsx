0d22b790bc3b80c79bd17f56a6ee075af3df3662
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-28 19:30:04 +0100

    change address functional

--- client/src/pages/profile/components/AddAddressMenu.tsx
+++ client/src/pages/profile/components/AddAddressMenu.tsx
@@ -11,13 +11,16 @@ type AddAddressMenuProps = {
 };
 
 type TypeFormData = {
-  email: string;
-  recipient: string;
-  street: string;
-  city: string;
-  country: string;
-  zip: undefined | number;
-  phone: string;
+  userId: string;
+  addressId: string,
+  addressData: {
+    recipient: string;
+    street: string;
+    city: string;
+    country: string;
+    zip: undefined | number;
+    phone: string;
+  }
 };
 
 type TypeAddressData = {
@@ -36,13 +39,16 @@ function AddAddressMenu({
 }: AddAddressMenuProps) {
   const user = useSelector(userDataSelector);
   const [formData, setFormData] = useState<TypeFormData>({
-    email: user.email,
-    recipient: "",
-    street: "",
-    city: "",
-    country: "",
-    zip: undefined,
-    phone: "",
+    userId: user.id,
+    addressId: "",
+    addressData: {
+      recipient: "",
+      street: "",
+      city: "",
+      country: "",
+      zip: undefined,
+      phone: "",
+    }
   });
   const [addresses, setAddresses] = useState([])
 
@@ -50,23 +56,46 @@ function AddAddressMenu({
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      addressData: {
+        ...prevData.addressData,
+        [name]: value,
+      },
     }));
+
   };
 
   const handleSubmit = async () => {
     try {
       await AddressServices.sendAddress(
-        formData.email,
-        formData.recipient,
-        formData.street,
-        formData.city,
-        formData.country,
-        formData.zip,
-        formData.phone
+          formData.userId,
+          formData.addressData.recipient,
+          formData.addressData.street,
+          formData.addressData.city,
+          formData.addressData.country,
+          formData.addressData.zip,
+          formData.addressData.phone
       );
       toast.success("Address add successfully");
-
+    } catch (error) {
+      console.error("Error sending address:", error);
+      toast.error("Error to adding the address");
+    }
+  };
+  const handleSubmitChanges = async (e) => {
+    e.preventDefault();
+    console.log(formData)
+    try {
+      await AddressServices.updateAddress(
+          formData.userId,
+          formData.addressId,
+          formData.addressData.recipient,
+          formData.addressData.street,
+          formData.addressData.city,
+          formData.addressData.country,
+          formData.addressData.zip,
+          formData.addressData.phone
+      );
+      toast.success("Address add successfully");
     } catch (error) {
       console.error("Error sending address:", error);
       toast.error("Error to adding the address");
@@ -83,109 +112,191 @@ function AddAddressMenu({
     fetchData();
   }, []);
 
-  return selectedLabel === "Add Address" ? (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "5px",
-        justifyContent: "space-between",
-        height: "100%",
-      }}
-    >
-      <div className={styles.title_action}>
-        <h2 className={styles.box_name}>ADDRESS</h2>
-        <button onClick={() => setSelectedLabel("Address")}>Return</button>
-      </div>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          placeholder="Recipients name"
-          className={styles.input}
-          type="text"
-          name="recipient"
-          value={formData.recipient}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="Street"
-          className={styles.input}
-          type="text"
-          name="street"
-          value={formData.street}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="City"
-          className={styles.input}
-          type="text"
-          name="city"
-          value={formData.city}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="Country"
-          className={styles.input}
-          type="text"
-          name="country"
-          value={formData.country}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="ZIP Code"
-          className={styles.input}
-          type="text"
-          name="zip"
-          value={formData.zip}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="Phone number"
-          className={styles.input}
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
-        <button className={styles.sendbutton} type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  ) : (
-    selectedLabel === "Address" && (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: 'center',
-          justifyContent: "space-between",
-          height: "100%",
-
-        }}
-      >
-        <div className={styles.title_action}>
-          <h2 className={styles.box_name}>ADDRESS</h2>
-          <button onClick={() => setSelectedLabel("Add Address")}>
-            + Add New Address
-          </button>
-        </div>
-        <div className={styles.address_content}>
-          <div className={styles.address_box}>
-            {addresses.map((address: TypeAddressData) =>
-               <div className={styles.address}>
-                 <div className={styles.address_text}>
-                   <p>{address.country}, {address.city}</p>
-                   <p>{address.street_address}, {address.ZIP_code} </p>
-                   <p>{address.recipients_name}</p>
-                 </div>
-                 <button>EDIT</button>
-               </div>
-            )}
+  if(selectedLabel === "Add Address"){
+    return (
+        <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+              justifyContent: "space-between",
+              height: "100%",
+            }}
+        >
+          <div className={styles.title_action}>
+            <h2 className={styles.box_name}>ADDRESS</h2>
+            <button onClick={() => setSelectedLabel("Address")}>Return</button>
           </div>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <input
+                placeholder="Recipients name"
+                className={styles.input}
+                type="text"
+                name="recipient"
+                value={formData.addressData.recipient}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Street"
+                className={styles.input}
+                type="text"
+                name="street"
+                value={formData.addressData.street}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="City"
+                className={styles.input}
+                type="text"
+                name="city"
+                value={formData.addressData.city}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Country"
+                className={styles.input}
+                type="text"
+                name="country"
+                value={formData.addressData.country}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="ZIP Code"
+                className={styles.input}
+                type="text"
+                name="zip"
+                value={formData.addressData.zip}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Phone number"
+                className={styles.input}
+                type="text"
+                name="phone"
+                value={formData.addressData.phone}
+                onChange={handleChange}
+            />
+            <button className={styles.sendbutton} type="submit">
+              Submit
+            </button>
+          </form>
         </div>
-      </div>
     )
-  );
+  }
+
+  if(selectedLabel === "Address"){
+    return (
+        selectedLabel === "Address" && (
+            <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: 'center',
+                  justifyContent: "space-between",
+                  height: "100%",
+
+                }}
+            >
+              <div className={styles.title_action}>
+                <h2 className={styles.box_name}>ADDRESS</h2>
+                <button onClick={() => setSelectedLabel("Add Address")}>
+                  + Add New Address
+                </button>
+              </div>
+              <div className={styles.address_content}>
+                <div className={styles.address_box}>
+                  {addresses.map((address: TypeAddressData) =>
+                      <div className={styles.address}>
+                        <div className={styles.address_text}>
+                          <p>{address.country}, {address.city}</p>
+                          <p>{address.street_address}, {address.ZIP_code} </p>
+                          <p>{address.recipients_name}</p>
+                        </div>
+                        <button onClick={() => {
+                          formData.addressId = address._id;
+                          setSelectedLabel('Edit Address')
+                        }}>EDIT</button>
+                      </div>
+                  )}
+                </div>
+              </div>
+            </div>
+        )
+    )
+  }
+
+  if(selectedLabel === "Edit Address"){
+    return (
+        <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+              justifyContent: "space-between",
+              height: "100%",
+            }}
+        >
+          <div className={styles.title_action}>
+            <h2 className={styles.box_name}>EDIT</h2>
+            <button onClick={() => setSelectedLabel("Address")}>Return</button>
+          </div>
+          <form className={styles.form} onSubmit={handleSubmitChanges}>
+            <input
+                placeholder="Recipients name"
+                className={styles.input}
+                type="text"
+                name="recipient"
+                value={formData.addressData.recipient}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Street"
+                className={styles.input}
+                type="text"
+                name="street"
+                value={formData.addressData.street}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="City"
+                className={styles.input}
+                type="text"
+                name="city"
+                value={formData.addressData.city}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Country"
+                className={styles.input}
+                type="text"
+                name="country"
+                value={formData.addressData.country}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="ZIP Code"
+                className={styles.input}
+                type="text"
+                name="zip"
+                value={formData.addressData.zip}
+                onChange={handleChange}
+            />
+            <input
+                placeholder="Phone number"
+                className={styles.input}
+                type="text"
+                name="phone"
+                value={formData.addressData.phone}
+                onChange={handleChange}
+            />
+            <button className={styles.sendbutton} type="submit">
+              Edit
+            </button>
+          </form>
+        </div>
+    )
+  }
+
 }
 
 export default AddAddressMenu
