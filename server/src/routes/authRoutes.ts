f35e6d8d52decfd718338779d121620e24a8106b
sappChak <konotop401@gmail.com>
2023-12-30 13:53:42 +0100

    Add spacing to improve readability in authRoutes.ts
    In an effort to increase code readability and maintainability, spacing has been added between the different route handlers in the authRoutes.ts file. This aims to better differentiate and highlight the specific route handler methods, making the code easier to navigate and understand.

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,8 +4,11 @@ import {Router} from "express";
 export const authRoutes: Router = Router();
 
 authRoutes.post("/signup", userController.signup);
+
 authRoutes.post("/login", userController.login);
+
 authRoutes.post("/logout", userController.logout);
+
 authRoutes.get("/refresh", userController.refresh);
 
 authRoutes.get("/tokens/oauth/google", userController.googleOauthHandler);
