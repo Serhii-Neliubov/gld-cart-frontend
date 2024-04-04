4bc375eee761465f435b4be3c64dfdcbbbaad714
sappChak <konotop401@gmail.com>
2024-04-04 16:54:19 +0200

    Update cartItem interface

--- src/utils/models/ICartItem.ts
+++ src/utils/models/ICartItem.ts
@@ -1,14 +1,9 @@
 export type TypeCartItem = {
   product: {
-    reviews: [];
     price: number;
     _id: string;
     product_name: string;
-    category: string;
-    subcategory: string;
-    description: string;
     images: string[];
-    attributes: object;
   };
   quantity: number;
   _id: string;
