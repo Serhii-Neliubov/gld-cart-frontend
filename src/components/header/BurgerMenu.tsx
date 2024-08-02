ee57051a09ef30dfd56fe7d9c6494ded4b05e725
serhiineliubov <serhii.neliubov@wbpo.sk>
2024-08-02 19:03:31 +0200

    fix(header): z-index

--- src/components/header/BurgerMenu.tsx
+++ src/components/header/BurgerMenu.tsx
@@ -6,7 +6,7 @@ interface BurgerMenuProps {
 
 export const BurgerMenu = ({setIsBurgerMenuOpen}: BurgerMenuProps) => {
     return (
-        <div onClick={() => setIsBurgerMenuOpen(false)} className={'absolute w-full h-full bg-opacity-80 z-10 bg-black top-0 left-0'}>
+        <div onClick={() => setIsBurgerMenuOpen(false)} className={'absolute w-full h-full bg-opacity-80 z-30 bg-black top-0 left-0'}>
             <div className={'min-h-[50px] flex-col w-full py-6 gap-[40px] text-center flex justify-center items-center bg-[#373a47]'}>
                 <Link to={'/'}>
                     <a className={'text-white font-medium text-[36px]'}>Home</a>
