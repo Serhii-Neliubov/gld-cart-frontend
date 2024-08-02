416c3e9abf6bf9ce22ca1e54905afa17e9540af9
serhiineliubov <serhii.neliubov@wbpo.sk>
2024-08-02 19:05:06 +0200

    fix(burger menu): added stopPropagation()

--- src/components/header/BurgerMenu.tsx
+++ src/components/header/BurgerMenu.tsx
@@ -7,7 +7,7 @@ interface BurgerMenuProps {
 export const BurgerMenu = ({setIsBurgerMenuOpen}: BurgerMenuProps) => {
     return (
         <div onClick={() => setIsBurgerMenuOpen(false)} className={'absolute w-full h-full bg-opacity-80 z-30 bg-black top-0 left-0'}>
-            <div className={'min-h-[50px] flex-col w-full py-6 gap-[40px] text-center flex justify-center items-center bg-[#373a47]'}>
+            <div onClick={event => event.stopPropagation()} className={'min-h-[50px] flex-col w-full py-6 gap-[40px] text-center flex justify-center items-center bg-[#373a47]'}>
                 <Link to={'/'}>
                     <a className={'text-white font-medium text-[36px]'}>Home</a>
                 </Link>
